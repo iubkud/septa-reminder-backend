3f15661906b65abe3139ed5b768f24351449b8b3
iubkud <mbarry@gmail.com>
2018-01-27 14:42:10 -0500

    added twilio messenger

---
+++ app/services/twilio_text_messenger.rb
@@ -0,0 +1,17 @@
+class TwilioTextMessenger
+  attr_reader :message, :number
+
+  def initialize(message, number)
+    @message = message
+    @number = number
+  end
+
+  def call
+    client = Twilio::REST::Client.new
+    client.messages.create({
+      from: Rails.application.secrets.twilio_phone_number,
+      to: @number,
+      body: @message
+    })
+  end
+end
